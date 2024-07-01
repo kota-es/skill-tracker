fd9f358c6d543cceb5fccd920e61bc346325bb3d
kota-es <info@k-3110.com>
2024-07-01 23:55:02 +0900

    feat: add toast

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -4,11 +4,29 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { getToast } from "remix-toast";
+import { ToastContainer, toast as notify } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./styles/reset.css";
+import { useEffect } from "react";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { toast, headers } = await getToast(request);
+  return json({ toast }, { headers });
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { toast } = useLoaderData<typeof loader>();
+  useEffect(() => {
+    if (toast) {
+      notify(toast.message, { type: toast.type });
+    }
+  }, [toast]);
+
   return (
     <html lang="en">
       <head>
@@ -21,6 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <ToastContainer />
       </body>
     </html>
   );
