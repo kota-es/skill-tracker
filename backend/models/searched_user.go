772442c0891a93545c72b2288a046e5da5e8021b
kota-es <info@k-3110.com>
2024-08-04 17:44:21 +0900

    fix: modify response field

--- backend/models/searched_user.go
+++ backend/models/searched_user.go
@@ -2,8 +2,8 @@ package models
 
 type SearchedUser struct {
 	UserID        int         `json:"user_id"`
-	Firstname     string      `json:"first_name"`
-	Lastname      string      `json:"last_name"`
+	Firstname     string      `json:"firstname"`
+	Lastname      string      `json:"lastname"`
 	FirstnameKana string      `json:"firstname_kana"`
 	LastnameKana  string      `json:"lastname_kana"`
 	Profile       UserProfile `json:"profile"`
